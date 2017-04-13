b943de7add9d0f98ac2b2b005eedcfa7b425cd2c
ZarsBranchkin <zars15@inbox.lv>
2017-04-13 15:53:12 +0300

    :sparkles: AnimateProperty API intent

--- Animations.loc.lua
+++ Animations.loc.lua
@@ -9,7 +9,7 @@ local liveAnimations = {}
 local animationQueue = {} -- Required for syncronizing animations with update thread
 
 local function newAnimation(animFun, duration, params)
-  assert(animFun, "Argument #1 expects animator function")
+  assert(animFun, "newAnimation argument #1: Function expected")
 
   local animation = {}
 
@@ -25,7 +25,7 @@ local function newAnimation(animFun, duration, params)
   mt.__tostring = "Freya animation object"
   mt.__metatable = "Locked Metatable: Freya"
   mt.__newindex = function(t, i, v)
-    assert(rawget(animation, i), ("Property %s doesn't exist!"):format(tostring(i)))
+    assert(rawget(animation, i), ("Animation: Property %s doesn't exist"):format(tostring(i)))
     rawset(animation, i, v)
   end
 
@@ -34,6 +34,38 @@ local function newAnimation(animFun, duration, params)
   return proxy
 end
 
+-- API
+function AnimateProperty(isLocal, object, property, to, from, duration)
+  assert(object and typeof(object) == "Instance", "Animations.AnimateProperty intent argument #1: Instance expected")
+  assert(typeof(property) == "String" and object[property], "Animations.AnimateProperty intent argument #2: Valid property expected")
+  assert(to, "Animations.AnimateProperty intent argument #3: Variable expected")
+
+  from = from or object[property]
+  duration = duration or 1
+
+  local propType = typeof(to)
+
+  assert(typeof(from) == propType, "Animations.AnimateProperty: From and to types does not match")
+
+  local animator
+
+  if propType == "number" then
+    animator = function(alpha)
+      object[property] = from + (to - from)*alpha
+    end
+  elseif propType == "Vector3" or propType == "CFrame" or propType == "Color3" then
+    animator = function(alpha)
+      object[property] = from:lerp(to, alpha)
+    end
+  else
+    error(("Animations.AnimateProperty: Unsupported property type %s"):format(propType))
+  end
+
+  newAnimation(animator, duration)
+end
+Intents:Register("Animations.AnimateProperty", ClientAnimateProperty)
+
+-- Update loop
 local function updateAnimations()
   local currentTick = tick()
   local dt = currentTick - lastTick
@@ -66,4 +98,3 @@ local function updateAnimations()
   end
 end
 game:GetService("RunService"):BindToRenderStep("FreyaAnimations", Enum.RenderPriority.Camera.Value + 1, updateAnimations)
-
