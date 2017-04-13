07b2a0680dfedd31d2f4e020db00028c4d9cab21
ZarsBranchkin <zars15@inbox.lv>
2017-04-13 15:11:33 +0300

    :construction: Animation update loop

--- Animations.loc.lua
+++ Animations.loc.lua
@@ -1,8 +1,12 @@
 local Freya = _G.Freya
 local Intents = Freya:GetComponent "Intents"
 
-local LiveAnimations = {}
-local AnimationQueue = {}
+local pop = table.remove
+local tick = tick
+local lastTick = tick()
+
+local liveAnimations = {}
+local animationQueue = {} -- Required for syncronizing animations with update thread
 
 local function newAnimation(animFun, duration, params)
   assert(animFun, "Argument #1 expects animator function")
@@ -20,9 +24,46 @@ local function newAnimation(animFun, duration, params)
   mt.__index = animation
   mt.__tostring = "Freya animation object"
   mt.__metatable = "Locked Metatable: Freya"
+  mt.__newindex = function(t, i, v)
+    assert(rawget(animation, i), ("Property %s doesn't exist!"):format(tostring(i)))
+    rawset(animation, i, v)
+  end
 
-  table.insert(AnimationQueue, proxy)
+  table.insert(animationQueue, proxy)
 
   return proxy
 end
+
+local function updateAnimations()
+  local currentTick = tick()
+  local dt = currentTick - lastTick
+  lastTick = currentTick
+
+  -- Activate queued animations
+  while #animationQueue > 0 do
+    liveAnimations[#liveAnimations] = pop(animationQueue)
+  end
+
+  for i=1,#liveAnimations do
+    local anim = liveAnimations[i]
+    anim.t = anim.t + dt
+
+    if animt.t >= anim.duration then
+      anim.t = anim.duration
+      anim.dead = true
+    end
+
+    anim:update(anim.t/anim.duration, dt)
+  end
+
+  -- Destroy dead animations
+  local offset = 0
+  for i=1,#liveAnimations do
+    if liveAnimations[i-offset].dead then
+      pop(liveAnimations, i-offset)
+      offset = offset + 1
+    end
+  end
+end
+game:GetService("RunService"):BindToRenderStep("FreyaAnimations", Enum.RenderPriority.Camera.Value + 1, updateAnimations)
 
